6d8f679e81b46c96f16105c373df37842d3f4c28
Delilah Lopez <Delilah.marinaL@gmail.com>
2025-09-08 15:03:54 -0400

    Sorted

--- index.js
+++ index.js
@@ -5,7 +5,13 @@
 // Write a function that returns true if the array is sorted in ascending order, false otherwise.
 
 function checkIfSorted(arr) {
-  // Your code here
+  //input arr
+  const sorted = arr.slice().sort((a,b)=> a-b).join();//create a var to hold sorted array join?
+  console.log(sorted);
+  console.log(arr.join());
+  return arr.join()=== sorted//compare the input arr against sorted 
+  //check if sorted
+  //output bool
 }
 
 // --- Starter Test Cases ---
@@ -14,3 +20,6 @@ console.log(checkIfSorted([1, 3, 2, 4])); // false
 
 // --- Your Turn ---
 // Add at least 2 more test cases and explain them out loud in your recording.
+
+console.log(checkIfSorted([100, 24, 33, 47])); 
+console.log(checkIfSorted([25,50,75,100]));
